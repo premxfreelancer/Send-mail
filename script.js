b428871e6dfd47bf59059b9285f077d081346c12
Prem <premruidas123@gmail.com>
2023-12-31 14:33:07 +0530

    all done

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ function submitForm(event) {
     // Redirect to Gmail with pre-filled email
     window.location.href = mailtoLink;
 }
+
+// Function to save form values in local storage
+function saveFormValues() {
+    const title = document.getElementById('title').value;
+    const body = document.getElementById('body').value;
+    const recipient = document.getElementById('recipient').value;
+
+    // Save form values in local storage
+    localStorage.setItem('savedTitle', title);
+    localStorage.setItem('savedBody', body);
+    localStorage.setItem('savedRecipient', recipient);
+}
+
+// Function to load saved values from local storage
+function loadSavedValues() {
+    const savedTitle = localStorage.getItem('savedTitle');
+    const savedBody = localStorage.getItem('savedBody');
+    const savedRecipient = localStorage.getItem('savedRecipient');
+
+    if (savedTitle) document.getElementById('title').value = savedTitle;
+    if (savedBody) document.getElementById('body').value = savedBody;
+    if (savedRecipient) document.getElementById('recipient').value = savedRecipient;
+}
+
+// Attach event listeners to the input fields
+document.getElementById('title').addEventListener('input', saveFormValues);
+document.getElementById('body').addEventListener('input', saveFormValues);
+document.getElementById('recipient').addEventListener('input', saveFormValues);
+
+// Call the loadSavedValues function when the page loads
+window.onload = loadSavedValues;
